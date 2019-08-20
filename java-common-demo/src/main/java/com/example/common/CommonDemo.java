3bb1ba365c5ba920f5eeaa6e3ae3a9f4b8c3f4c4
githubshenyuan <3108065@qq.com>
2019-08-20 22:28:45 +0800

    Update CommonDemo.java

--- java-common-demo/src/main/java/com/example/common/CommonDemo.java
+++ java-common-demo/src/main/java/com/example/common/CommonDemo.java
@@ -2,13 +2,36 @@ package com.example.common;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CommonDemo {
 
     public static void main(String[] args) {
-        Map map = new HashMap();
-        map.put("aaa", "aaa");
-        System.out.println(map);
+        System.out.println("");
+
+        HashMap hashMap = new HashMap();
+        hashMap.put("1", "aaa");
+        hashMap.put("2", "aaa");
+        hashMap.put("3", "aaa");
+        hashMap.put("4", "aaa");
+        hashMap.put("5", "aaa");
+        hashMap.put("6", "aaa");
+        hashMap.put("7", "aaa");
+        hashMap.put("8", "aaa");
+        hashMap.put("9", "aaa");
+        hashMap.put("10", "aaa");
+        hashMap.put("11", "aaa");
+        hashMap.put("12", "aaa");
+        hashMap.put("13", "aaa");
+        hashMap.put("14", "aaa");
+        hashMap.put("15", "aaa");
+        hashMap.put("16", "aaa");
+        hashMap.put("17", "aaa");
+
+        System.out.println(hashMap);
+
+        ConcurrentHashMap map = new ConcurrentHashMap();
+        map.put("", "");
 
         System.out.println("");
 
