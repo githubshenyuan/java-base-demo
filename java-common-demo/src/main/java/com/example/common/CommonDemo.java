5ed43808fec6dfc92c9a0d3e8a3ac524a5f706cf
githubshenyuan <35088383+githubshenyuan@users.noreply.github.com>
2019-08-20 18:03:31 +0800

    提交

---
+++ java-common-demo/src/main/java/com/example/common/CommonDemo.java
@@ -0,0 +1,16 @@
+package com.example.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonDemo {
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("aaa", "aaa");
+        System.out.println(map);
+
+        System.out.println("");
+
+    }
+}
