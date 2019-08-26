15fce8f075f82ba34bef4c55598449b7c7c3b2de
githubshenyuan <35088383+githubshenyuan@users.noreply.github.com>
2019-08-27 07:52:49 +0800

    提交

--- java-sort-demo/src/main/java/com/example/sort/QuickSort.java
+++ java-sort-demo/src/main/java/com/example/sort/QuickSort.java
@@ -3,21 +3,35 @@ package com.example.sort;
 public class QuickSort {
     public static void main(String[] args) {
         int[] sort = {3, 8, 9, 2, 5, 4, 0, 1, 7};
+        quick(sort, 0, sort.length-1);
+        for (int i : sort) {
+            System.out.println(i);
+        }
+    }
+
+
+    public static void quick(int[] sort, int low, int high) {
+        if (low < high) {
+            int re = getIndex(sort, low, high);
+            quick(sort, low, re-1);
+            quick(sort, re + 1, high);
+        }
     }
 
     public static int getIndex(int[] sort, int low, int high) {
-        int tem = sort[low];
+        int tmp = sort[low];
         while (low < high) {
-            while (low < high && sort[high] >= tem) {
+            while (low < high && sort[high] >= tmp) {
                 high--;
             }
             sort[low] = sort[high];
-            while (low < high && sort[low] <= tem) {
+            while (low < high && sort[low] <= tmp) {
                 low++;
             }
+            sort[high] = sort[low];
         }
-
-        return 1;
-
+        sort[low] = tmp;
+        return low;
     }
+
 }
---
+++ java-sort-demo/src/main/java/com/example/sort/QuickVariantSort.java
@@ -0,0 +1,16 @@
+package com.example.sort;
+
+public class QuickVariantSort {
+
+
+    public static int getPartition(int sort ,int start ,int end) {
+        int pivot = (int)()
+
+    }
+
+    public static void swap(int[] sort, int i, int j) {
+        int tmp = sort[i];
+        sort[i] = sort[j];
+        sort[j] = tmp;
+    }
+}
